46e6202d49a2615fa7ce70b3bd1424df0c74867a
ZH_ubuntu <myonly2015@163.com>
2022-02-26 22:16:06 +0800

    add Push & Slide log with video

--- train0219.sh
+++ train0219.sh
@@ -1,3 +1,3 @@
-mpirun -np 8 python -u train.py --env-name='FetchPush-v1' --n-epochs='200' 2>&1 | tee push0219.log
-mpirun -np 16 python -u train.py --env-name='FetchPickAndPlace-v1' --n-epochs='500' 2>&1 | tee pick0220.log
+mpirun -np 8 python -u train.py --env-name='FetchPush-v1' --n-epochs='400' 2>&1 | tee push0219.log
+mpirun -np 8 python -u train.py --env-name='FetchSlide-v1' --n-epochs='500' 2>&1 | tee slide0220.log
 
